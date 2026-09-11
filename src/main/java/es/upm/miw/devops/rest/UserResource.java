e4d14e0af45de9358228ae9d7ba484ad3b7aa706
Milen Rodriguez Man <milen.rodriguezman@ucr.ac.cr>
2026-09-11 18:52:25 +0200

    Add GET /user/{id} endpoint #7

---
+++ src/main/java/es/upm/miw/devops/rest/UserResource.java
@@ -0,0 +1,28 @@
+package es.upm.miw.devops.rest;
+
+import es.upm.miw.devops.rest.dto.UserResponse;
+import es.upm.miw.devops.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(UserResource.USER)
+public class UserResource {
+
+    public static final String USER = "/user";
+
+    private final UserService userService;
+
+    public UserResource(UserService userService)
+    {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public UserResponse findById(@PathVariable Long id)
+    {
+        return UserResponse.from(userService.findById(id));
+    }
+}
