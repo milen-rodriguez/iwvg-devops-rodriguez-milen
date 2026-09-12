13951003e0677cb5ada10ba169e23f071874cc80
Milen Rodriguez Man <milen.rodriguezman@ucr.ac.cr>
2026-09-12 11:22:22 +0200

    Configure spotless formatter #18

--- src/main/java/es/upm/miw/devops/persistence/UserRepository.java
+++ src/main/java/es/upm/miw/devops/persistence/UserRepository.java
@@ -3,5 +3,4 @@ package es.upm.miw.devops.persistence;
 import es.upm.miw.devops.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-}
+public interface UserRepository extends JpaRepository<User, Long> {}
