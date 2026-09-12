ea570d4273c38d89894e004688768fa514869e46
Milen Rodriguez Man <milen.rodriguezman@ucr.ac.cr>
2026-09-12 12:52:31 +0200

    Add tests for GET /user/{id} endpoint #8

---
+++ src/test/java/es/upm/miw/devops/integrationtests/UserServiceIT.java
@@ -0,0 +1,40 @@
+package es.upm.miw.devops.integrationtests;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import es.upm.miw.devops.persistence.UserRepository;
+import es.upm.miw.devops.service.NotFoundException;
+import es.upm.miw.devops.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class UserServiceIT {
+
+  @Autowired private UserService userService;
+
+  @Autowired private UserRepository userRepository;
+
+  @Test
+  void shouldFindSeededUserById() {
+    assertThat(userService.findById(1L).getEmail()).isEqualTo("ada.lovelace@example.com");
+  }
+
+  @Test
+  void shouldDeleteSeededUserById() {
+    userService.deleteById(4L);
+
+    assertThat(userRepository.findById(4L)).isEmpty();
+  }
+
+  @Test
+  void shouldThrowNotFoundExceptionWhenDeletingUnknownUser() {
+    assertThatThrownBy(() -> userService.deleteById(999L))
+        .isInstanceOf(NotFoundException.class)
+        .hasMessage("User with id 999 was not found");
+  }
+}
