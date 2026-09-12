bd10d87a09732ac708641002cd0b8bba51c1e980
Milen Rodriguez Man <milen.rodriguezman@ucr.ac.cr>
2026-09-12 13:08:32 +0200

    Add PUT /user/{id}/active endpoint #11

--- src/main/java/es/upm/miw/devops/rest/UserResource.java
+++ src/main/java/es/upm/miw/devops/rest/UserResource.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +32,11 @@ public class UserResource {
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteById(@PathVariable Long id) {
     userService.deleteById(id);
+  }
+
+  @PutMapping("/{id}/active")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void activateById(@PathVariable Long id) {
+    userService.activateById(id);
   }
 }
