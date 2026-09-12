324e58c780327145602b343d43aad214ce0f70bc
Milen Rodriguez Man <milen.rodriguezman@ucr.ac.cr>
2026-09-12 12:02:39 +0200

    Add DELETE /user/{id} endpoint #9

--- src/main/java/es/upm/miw/devops/rest/UserResource.java
+++ src/main/java/es/upm/miw/devops/rest/UserResource.java
@@ -2,9 +2,12 @@ package es.upm.miw.devops.rest;
 
 import es.upm.miw.devops.rest.dto.UserResponse;
 import es.upm.miw.devops.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +25,11 @@ public class UserResource {
   @GetMapping("/{id}")
   public UserResponse findById(@PathVariable Long id) {
     return UserResponse.from(userService.findById(id));
+  }
+
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteById(@PathVariable Long id) {
+    userService.deleteById(id);
   }
 }
