2f06293d41149d19cb5e54e4ad895bc1d0c47398
Milen Rodriguez Man <milen.rodriguezman@ucr.ac.cr>
2026-09-12 12:52:31 +0200

    Add tests for GET /user/{id} endpoint #8

---
+++ src/test/resources/data.sql
@@ -0,0 +1,87 @@
+INSERT INTO "user" (
+    id,
+    first_name,
+    family_name,
+    email,
+    identity,
+    address,
+    city,
+    province,
+    postal_code
+) VALUES (
+    1,
+    'Ada',
+    'Lovelace',
+    'ada.lovelace@example.com',
+    'ID-1',
+    '1 Analytical Engine Way',
+    'London',
+    'London',
+    'N1 1AA'
+);
+
+INSERT INTO "user" (
+    id,
+    first_name,
+    family_name,
+    email,
+    identity,
+    address,
+    city,
+    province,
+    postal_code
+) VALUES (
+    2,
+    'Grace',
+    'Hopper',
+    'grace.hopper@example.com',
+    'ID-2',
+    '2 Compiler Street',
+    'New York',
+    'New York',
+    '10001'
+);
+
+INSERT INTO "user" (
+    id,
+    first_name,
+    family_name,
+    email,
+    identity,
+    address,
+    city,
+    province,
+    postal_code
+) VALUES (
+    3,
+    'Alan',
+    'Turing',
+    'alan.turing@example.com',
+    'ID-3',
+    '3 Enigma Road',
+    'London',
+    'London',
+    'N1 1AB'
+);
+
+INSERT INTO "user" (
+    id,
+    first_name,
+    family_name,
+    email,
+    identity,
+    address,
+    city,
+    province,
+    postal_code
+) VALUES (
+    4,
+    'Edsger',
+    'Dijkstra',
+    'edsger.dijkstra@example.com',
+    'ID-4',
+    '4 Algorithm Avenue',
+    'Eindhoven',
+    'North Brabant',
+    '5611 AA'
+);
