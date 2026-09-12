69ed4ccc15ca39a86efb59ed57e7c8446f5f3040
Milen Rodriguez Man <milen.rodriguezman@ucr.ac.cr>
2026-09-12 11:22:22 +0200

    Configure spotless formatter #18

--- src/main/java/es/upm/miw/devops/service/NotFoundException.java
+++ src/main/java/es/upm/miw/devops/service/NotFoundException.java
@@ -2,8 +2,7 @@ package es.upm.miw.devops.service;
 
 public class NotFoundException extends RuntimeException {
 
-    public NotFoundException(Class<?> entityType, Object id)
-    {
-        super(entityType.getSimpleName() + " with id " + id + " was not found");
-    }
+  public NotFoundException(Class<?> entityType, Object id) {
+    super(entityType.getSimpleName() + " with id " + id + " was not found");
+  }
 }
