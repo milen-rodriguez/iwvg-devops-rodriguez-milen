d6270f03d248936a2bda40612e01b6ccd157750d
Milen Rodriguez Man <milen.rodriguezman@ucr.ac.cr>
2026-09-21 16:16:17 +0200

    Create PUT /user/{id} endpoint #45

--- src/main/java/es/upm/miw/devops/domain/User.java
+++ src/main/java/es/upm/miw/devops/domain/User.java
@@ -55,32 +55,64 @@ public class User {
     return firstName;
   }
 
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
   public String getFamilyName() {
     return familyName;
+  }
+
+  public void setFamilyName(String familyName) {
+    this.familyName = familyName;
   }
 
   public String getEmail() {
     return email;
   }
 
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
   public String getIdentity() {
     return identity;
+  }
+
+  public void setIdentity(String identity) {
+    this.identity = identity;
   }
 
   public String getAddress() {
     return address;
   }
 
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
   public String getCity() {
     return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
   }
 
   public String getProvince() {
     return province;
   }
 
+  public void setProvince(String province) {
+    this.province = province;
+  }
+
   public String getPostalCode() {
     return postalCode;
+  }
+
+  public void setPostalCode(String postalCode) {
+    this.postalCode = postalCode;
   }
 
   public boolean isActive() {
--- src/main/java/es/upm/miw/devops/rest/UserResource.java
+++ src/main/java/es/upm/miw/devops/rest/UserResource.java
@@ -1,12 +1,16 @@
 package es.upm.miw.devops.rest;
 
+import es.upm.miw.devops.rest.dto.UserRequest;
 import es.upm.miw.devops.rest.dto.UserResponse;
+import es.upm.miw.devops.rest.mapper.UserMapper;
 import es.upm.miw.devops.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,15 +21,24 @@ public class UserResource {
 
   public static final String USER = "/user";
 
+  private final UserMapper userMapper;
   private final UserService userService;
 
-  public UserResource(UserService userService) {
+  public UserResource(UserMapper userMapper, UserService userService) {
+
+    this.userMapper = userMapper;
     this.userService = userService;
   }
 
   @GetMapping("/{id}")
   public UserResponse findById(@PathVariable Long id) {
-    return UserResponse.from(userService.findById(id));
+    return userMapper.toResponse(userService.findById(id));
+  }
+
+  @PutMapping("/{id}")
+  public UserResponse updateById(@PathVariable Long id, @Valid @RequestBody UserRequest request) {
+    return userMapper.toResponse(
+        userService.updatePersonalDataById(id, userMapper.toUpdateCommand(request)));
   }
 
   @DeleteMapping("/{id}")
